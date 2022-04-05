64acd76a091a0b72fece69a735bcbb1edf658303
Igormcf <igormaciel82@gmail.com>
2022-04-05 19:01:06 -0300

    Ends requirement 04

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class Game extends Component {
   render() {
     return (
-      <div>Game Page</div>
+      <div>
+        <h1>Game Page</h1>
+        <Header />
+      </div>
     );
   }
 }
--- src/reducers/player.js
+++ src/reducers/player.js
@@ -3,7 +3,7 @@ import { LOGIN } from '../actions';
 const INITIAL_STATE = {
   name: '',
   assertions: '',
-  score: '',
+  score: 0,
   gravatarEmail: '',
 };
 
