7c09b80567bbfbb0eebcf740bbea92fcb8bb4f3b
lucas <lucas.c.r.f@hotmail.com>
2022-04-07 18:28:24 -0300

    requsitos 14 e 15

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -5,26 +5,32 @@ import HeaderFeedback from '../components/HeaderFeedback';
 
 class Feedback extends Component {
   render() {
-    const { rightAnswers } = this.props;
+    const { rightAnswers, history, setScore } = this.props;
     const AVALIATOR = 3;
     return (
       <div>
         <HeaderFeedback />
-        { (rightAnswers < AVALIATOR) ? (
+        {(rightAnswers < AVALIATOR) ? (
           <h4 data-testid="feedback-text">Could be better...</h4>
         ) : (
           <h4 data-testid="feedback-text">Well Done!</h4>
         )}
-        {/* <button
+        <p>Placar Final:</p>
+        <p data-testid="feedback-total-score">
+          { setScore }
+        </p>
+        <p> Acertos: </p>
+        <p data-testid="feedback-total-question">
+          { rightAnswers }
+        </p>
+        <button
           type="button"
           data-testid="btn-play-again"
           onClick={ () => {
             history.push('/');
           } }
         >
-          {' '}
           JOGAR NOVAMENTE
-          {' '}
 
         </button>
         <button
@@ -36,7 +42,7 @@ class Feedback extends Component {
         >
           VER RANKING
 
-        </button> */}
+        </button>
       </div>
     );
   }
@@ -44,10 +50,15 @@ class Feedback extends Component {
 
 const mapStateToProps = (state) => ({
   rightAnswers: state.player.assertions,
+  setScore: state.player.score,
 });
 
 Feedback.propTypes = {
   rightAnswers: propTypes.number.isRequired,
+  setScore: propTypes.number.isRequired,
+  history: propTypes.shape({
+    push: propTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default connect(mapStateToProps, null)(Feedback);
